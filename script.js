d54748c30cf848b3655b9001b140d75d16588208
Mani Poorna <118202451+ManiPoorna@users.noreply.github.com>
2023-09-07 10:31:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ const addUrl = (e) => {
 	let name = document.getElementById("name").value;
 	let year = document.getElementById("year").value;
 	if(name!=="" && year!== ""){
-		url.innerText = `https://localhost:8080/name=${name}&year=${year}`
+		url.innerText = `https://localhost:8080/?name=${name}&year=${year}`
 	}
 	else if(name!=="" && year==""){
-		url.innerText = `https://localhost:8080/name=${name}`
+		url.innerText = `https://localhost:8080/?name=${name}`
 	}
 	else if(name=="" && year!==""){
-		url.innerText = `https://localhost:8080/year=${year}`
+		url.innerText = `https://localhost:8080/?year=${year}`
 	}
 	
 }

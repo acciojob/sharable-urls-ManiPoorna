29ea07b07a894027243c09e8c994d29b22fb7d94
Mani Poorna <118202451+ManiPoorna@users.noreply.github.com>
2023-09-07 10:22:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // your code here
+const span = document.getElementById("span");
+const addUrl = (e) => {
+	e.preventDefault();
+	let name = document.getElementById("name").value;
+	let year = document.getElementById("year").value;
+	if(name!=="" && year!== ""){
+		span.innerText = `name=${name}&year=${year}`
+	}
+	else if(name!=="" && year==""){
+		span.innerText = `name=${name}`
+	}
+	else if(name=="" && year!==""){
+		span.innerText = `year=${year}`
+	}
+	
+}
+
+
+
+
+
+
+
